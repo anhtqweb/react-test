0db081f0a4970b3d89b0eca34f28e76285ad7e40
qaqashy <tranquanganh99@gmail.com>
2024-05-12 10:30:55 -0400

    add button column

--- src/MyTable.js
+++ src/MyTable.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
+import { Button } from 'primereact/button';
+
+
 export default function MyTable() {
     const [products, setProducts] = useState([
         {"code": "hello", "name": "world", "category": "bye", "quantity": 4},
@@ -11,6 +14,19 @@ export default function MyTable() {
         {"code": "dfg", "name": "jytj", "category": "zxcv", "quantity": 44},
     ]);
 
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                <Button icon="pi pi-pencil" rounded outlined className="mr-2" 
+                // onClick={() => editProduct(rowData)} 
+                />
+                <Button icon="pi pi-trash" rounded outlined severity="danger" 
+                // onClick={() => confirmDeleteProduct(rowData)} 
+                />
+            </React.Fragment>
+        );
+    };
+
     return (
         <div className="card">
             <DataTable value={products} showGridlines tableStyle={{ minWidth: '50rem' }}>
@@ -18,6 +34,7 @@ export default function MyTable() {
                 <Column field="name" header="Name"></Column>
                 <Column field="category" header="Category"></Column>
                 <Column field="quantity" header="Quantity"></Column>
+                <Column body={actionBodyTemplate}></Column>
             </DataTable>
         </div>
     );
